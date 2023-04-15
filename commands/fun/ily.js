40212b2eb85fd1859e96b0047726aa0d3c94820a
Harley <51787264+Harleythetech@users.noreply.github.com>
2023-04-16 00:02:56 +0800

    Chiyoko Haruka: Initial Release v2.r002s
    Bot has the following features:
    - Spotify Status Checker
    - reddit stuff (r/yandere, r/meme, r/wholesomememe, r/suddenlygay, r/whataweeb)
    - For desperate people the bot has ILoveYou command
    Then some utilities
    - info 
    - invite
    - ping
    - serverinfo
    - userinfo

---
+++ commands/fun/ily.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports ={
+    data: new SlashCommandBuilder()
+    .setName('ily-confession')
+    .setDescription(`what? are you really that desperate?`),
+    async execute(interaction){
+        console.log(`[LOG] ily-confession has been executed`);
+        try{
+        const uname = interaction.user.username;
+        const ily = new EmbedBuilder()
+        .setTitle(`Get Rejected!`)
+        .setDescription(`${uname} I Don't Love you.`)
+        .setColor(0x0099ff)
+        .setImage(`https://c.tenor.com/HYvhvbB4QmYAAAAC/blegh-hahaha.gif`)
+        .setTimestamp()
+        .setFooter({text: `Time Rejected`})
+        return interaction.reply({embeds: [ily]})
+        }catch (error){
+            console.error(error)
+        }
+    }
+}
---
+++ handlers/embed.js
@@ -0,0 +1,20 @@
+const {Client,GatewayIntentBits, EmbedBuilder} = require('discord.js');
+const client = new Client ({intents:[GatewayIntentBits.Guilds],  disableMentions: "all" });
+const interaction = require('discord.js');
+//Bot Up
+const active = new EmbedBuilder()
+.setColor(0x0099ff)
+.setTitle(`Beep Boop...`)
+.setDescription(`Chiyoko Haruka is now active!`)
+.setImage('https://64.media.tumblr.com/02dabf2a7299753f442feee1512e326c/tumblr_o2qhk8IoJs1tydz8to1_500.gif')
+.setTimestamp()
+.setFooter({text: 'Chiyoko Haruka: V0.2.r002s'});
+//Error
+const bug = new EmbedBuilder()
+.setColor(0x0099ff)
+.setTitle(`Beep Boop...`)
+.setDescription(`Error! Failed to execute command (╯°□°）╯︵ ┻━┻ `)
+.setImage('https://c.tenor.com/_3mSq0fET5oAAAAC/tenor.gif')
+.setTimestamp()
+.setFooter({text: 'Chiyoko Haruka: V0.2.r002s'});
+module.exports = {active, bug};
